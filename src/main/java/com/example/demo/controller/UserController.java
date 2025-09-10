c7ca3beb7b53f7de995637368458df1af5ecca47
RahulPodicheti <23645a0517@vaagdevi.edu.in>
2025-09-10 21:45:48 +0530

    updated

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -14,7 +14,7 @@ import com.example.demo.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:5173") // your React Vite port
+@CrossOrigin(origins = "https://userreg-frontend.onrender.com") // your React Vite port
 public class UserController {
 
     @Autowired
