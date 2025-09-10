dfda3ef071842c9e3c92300c918fb38baad5e281
RahulPodicheti <23645a0517@vaagdevi.edu.in>
2025-09-10 15:38:59 +0530

    first commit

---
+++ src/main/java/com/example/demo/UserRegApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegApplication.class, args);
+		System.out.println("=============================RUNNING=============================");
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:5173") // your React Vite port
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Register new user
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // Optional: Get all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
